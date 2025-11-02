4231797dddd1c62457d877f70640a10aff0a0f76
dawid mrotek <DawidMrotek@protonmail.ch>
2025-11-02 19:04:49 +0100

    [STYLE] Notes field to Contact class

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -9,3 +9,4 @@ User = settings.AUTH_USER_MODEL
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField()
+    notes = models.TextField(blank=True, default="")
