7c8442b58969a4813e486c4777aba94c6b5698c1
dawid mrotek <DawidMrotek@protonmail.ch>
2025-11-04 01:56:10 +0100

    [STYLE] Django template

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-from django.shortcuts import redirect
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
 import os
 from pathlib import Path
 
@@ -15,9 +14,14 @@ def dashboard_webpage(request, *args, **kwargs):
     print(request.user, request.user.is_authenticated)
     if not request.user.is_authenticated:
         return redirect("/auth/google/login")
-    dashboard_html = Path(os.path.join(TEMPLATES_DIR, "dashboard.html"))
-    if not dashboard_html.exists():
-        return HttpResponse("Not Found", status=404)
-    dashboard_html_val = dashboard_html.read_text()
-    _html = dashboard_html_val.format(my_value=str(request.user))
-    return HttpResponse(_html)
+    my_value = str(request.user)
+    template_context = {"my_value": my_value}
+    return render(
+        request=request, template_name="dashboard.html", context=template_context
+    )
+    # dashboard_html = Path(os.path.join(TEMPLATES_DIR, "dashboard.html"))
+    # if not dashboard_html.exists():
+    #     return HttpResponse("Not Found", status=404)
+    # dashboard_html_val = dashboard_html.read_text()
+    # _html = dashboard_html_val.format(my_value=str(request.user))
+    # return HttpResponse(_html)
